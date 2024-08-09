28e9dc742fa3e7a9f2a3c7c54ccd34d82a698b99
I M A N E <imanebahy91@gmail.com>
2024-08-09 17:09:34 +0100

    docker file

--- src/test/java/Support/Service/SupportServiceApplicationTests.java
+++ src/test/java/Support/Service/SupportServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SupportServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
